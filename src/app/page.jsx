8f1efc746692cf96ee76bc39388e98bfaf14dbc3
beratberkayg <berat.1675@gmail.com>
2024-01-06 22:35:04 +0300

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,9 @@ import Background from "@/components/Background";
 
 const Home = () => {
   return (
-    <>
-      <Background />
-    </>
+    <div className="flex items-center justify-center w-full h-screen">
+      {/* <div className="shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md w-full h-[50%] "></div> */}
+    </div>
   );
 };
 
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,82 +1,20 @@
-"use client";
-import React, { useEffect } from "react";
-import * as THREE from "three";
-
-const randomArbitrary = (min, max) => {
-  return Math.random() * (max - min) + min;
-};
+import React from "react";
 
 const Background = () => {
-  useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      35,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-
-    const renderer = new THREE.WebGLRenderer({ antialias: false });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    let spheres = [];
-    for (let i = 1; i < 300; i++) {
-      let geometry = new THREE.SphereGeometry(
-        0.02 * randomArbitrary(0.5, 1),
-        6,
-        6
-      );
-      let material = new THREE.MeshBasicMaterial({
-        color: new THREE.Color(
-          1,
-          randomArbitrary(190, 220) / 255,
-          Math.round(Math.random())
-        ),
-      });
-
-      let sphere = new THREE.Mesh(geometry, material);
-      scene.add(sphere);
-      spheres.push(sphere);
-      sphere.position.setFromSpherical(
-        new THREE.Spherical(
-          5 + 5 * Math.random(),
-          2 * Math.PI * Math.random(),
-          2 * Math.PI * Math.random()
-        )
-      );
-    }
-    scene.rotation.x = 0.2;
-    scene.rotation.z = 0.6;
-    camera.position.z = 5;
-
-    const axesHelper = new THREE.AxesHelper(3);
-    scene.add(axesHelper);
-
-    const animate = () => {
-      requestAnimationFrame(animate);
-      scene.rotation.y += 0.001;
-      scene.rotation.z -= 0.001;
-
-      renderer.render(scene, camera);
-    };
-
-    animate();
-
-    const handleWindowResize = () => {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    };
-
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []); // Empty dependency array to ensure the effect runs once on mount
-
-  return null;
+  return (
+    <div className="bg bg-[#030014]  ">
+      <div className="h-full w-full flex items-center justify-center relative">
+        <video
+          autoPlay
+          muted
+          loop
+          className="rotate-180 absolute top-[0] left-0 z-[-1] w-full h-full object-cover "
+        >
+          <source src="/blackhole.webm" type="video/webm" />
+        </video>
+      </div>
+    </div>
+  );
 };
 
 export default Background;
