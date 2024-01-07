38e68685c17ae64b5351b5b5de6939166457b90b
beratberkayg <berat.1675@gmail.com>
2024-01-07 22:57:42 +0300

    three.js update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,8 @@ import Background from "@/components/Background";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center w-full h-screen">
-      {/* <div className="shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md w-full h-[50%] "></div> */}
+    <div>
+      <Background />
     </div>
   );
 };
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+"use client";
+import * as THREE from "three";
 
 const Background = () => {
+  const stars = [];
+
+  // Yıldızları oluştur
+  for (let i = 0; i < 1000; i++) {
+    const star = new THREE.Mesh(
+      new THREE.SphereGeometry(0.1, 10, 10),
+      new THREE.MeshBasicMaterial({ color: 0xffffff })
+    );
+
+    star.position.x = (Math.random() - 0.5) * 2000; // Rastgele x konumu
+    star.position.y = (Math.random() - 0.5) * 2000; // Rastgele y konumu
+    star.position.z = (Math.random() - 0.5) * 2000; // Rastgele z konumu
+
+    stars.push(star);
+  }
+
+  const update = (time) => {
+    // Zamanla yıldızları hareket ettir
+    stars.forEach((star) => {
+      star.rotation.x += 0.001;
+      star.rotation.y += 0.001;
+      star.rotation.z += 0.001;
+    });
+  };
   return (
-    <div className="bg bg-[#030014]  ">
-      <div className="h-full w-full flex items-center justify-center relative">
-        <video
-          autoPlay
-          muted
-          loop
-          className="rotate-180 absolute top-[0] left-0 z-[-1] w-full h-full object-cover "
-        >
-          <source src="/blackhole.webm" type="video/webm" />
-        </video>
-      </div>
-    </div>
+    <canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      {stars.map((star, index) => (
+        <primitive key={index} object={star} />
+      ))}
+      <group rotation={[0, 0, 0]} position={[0, 0, 0]}>
+        <mesh>
+          <sphereGeometry args={[0.5, 16, 16]} />
+          <meshBasicMaterial color="red" />
+        </mesh>
+      </group>
+    </canvas>
   );
 };
 
